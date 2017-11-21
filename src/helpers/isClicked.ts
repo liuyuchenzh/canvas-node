09e85f884411202ce92d917e8606824d72ef1963
Yuchen Liu <liuyuchenzh@163.com>
2017-11-21 16:13:40 +0800

    improve efficiency of getClickedLine

--- src/helpers/isClicked.ts
+++ src/helpers/isClicked.ts
@@ -2,10 +2,12 @@ import { CanvasNode, Pos } from '../node'
 import { ArrowNode } from '../arrow'
 import pointInPolygon from 'point-in-polygon'
 import { Manager } from '../manager'
-import {findFromRight} from './findFromRight'
+import { findFromRight } from './findFromRight'
+import { isPointOnBezierCurve } from './cubicCurve'
+import { isPointOnCurve } from './quadraticCurve'
 
 export type Poly = [number, number][]
-const MARGIN_ERROR = 4
+export const MARGIN_ERROR = 4
 
 /**
  * detect whether a point is in a polygon
@@ -46,70 +48,6 @@ function convertToPoly(vertexes: number[]): Poly {
 }
 
 /**
- * detect whether a point is on the track of a Quadratic Bézier curve
- * @param {Poly} poly
- * @param {Pos} pos
- * @returns {boolean}
- */
-export function isPointOnCurve(poly: Poly, pos: Pos): boolean {
-  if (poly.length !== 3) {
-    console.error('only support Quadratic Bézier curves for now')
-    return false
-  }
-  const { x, y } = pos
-  const [start, control, end] = poly
-  const [startX, startY] = start
-  const [controlX, controlY] = control
-  const [endX, endY] = end
-  // subjective number
-  const numOfTest: number =
-    Math.floor(distanceBetween2Points(startX, startY, endX, endY)) / 2
-  const inc: number = 1 / numOfTest
-  let t: number = inc
-  while (t < 1) {
-    const lineX: number = simulateCurve(startX, controlX, endX, t)
-    const lineY: number = simulateCurve(startY, controlY, endY, t)
-    if (distanceBetween2Points(x, y, lineX, lineY) < MARGIN_ERROR) return true
-    t += inc
-  }
-  return false
-}
-
-/**
- * get a point on Quadratic Bézier curve
- * @param {number} p0
- * @param {number} p1
- * @param {number} p2
- * @param {number} t
- * @returns {number}
- */
-export function simulateCurve(
-  p0: number,
-  p1: number,
-  p2: number,
-  t: number
-): number {
-  return Math.pow(1 - t, 2) * p0 + 2 * (1 - t) * t * p1 + Math.pow(t, 2) * p2
-}
-
-/**
- * get directive of the curve
- * @param {number} p0
- * @param {number} p1
- * @param {number} p2
- * @param {number} t
- * @returns {number}
- */
-export function getDirective(
-  p0: number,
-  p1: number,
-  p2: number,
-  t: number
-): number {
-  return 2 * (1 - t) * (p1 - p0) + 2 * t * (p2 - p1)
-}
-
-/**
  * get distance between two points
  * @param {number} x1
  * @param {number} y1
@@ -117,7 +55,7 @@ export function getDirective(
  * @param {number} y2
  * @returns {number}
  */
-function distanceBetween2Points(
+export function distanceBetween2Points(
   x1: number,
   y1: number,
   x2: number,
@@ -137,7 +75,16 @@ export function getClickedNode(pos: Pos): CanvasNode {
   return findFromRight(list, node => {
     if (!node.display) return false
     if (node instanceof ArrowNode) {
-      return isPointOnCurve(node.stops, pos)
+      if (Manager.useCubicBezier) {
+        const isValidLine: boolean =
+          node.from &&
+          node.to &&
+          node.from instanceof CanvasNode &&
+          node.to instanceof CanvasNode
+        return !isValidLine || isPointOnBezierCurve(node.pos, node.endPos, pos)
+      } else {
+        return isPointOnCurve(node.stops, pos)
+      }
     }
     if (!node.vertexes) return false
     return isPointInPolygon(node.vertexes, pos)
@@ -165,5 +112,39 @@ export function getClickedLine(pos: Pos): ArrowNode {
   const list: ArrowNode[] = <ArrowNode[]>Manager.list.filter(
     node => node instanceof ArrowNode && node.display
   )
-  return findFromRight(list, node => isPointOnCurve(node.stops, pos))
+  return findFromRight(list, node => {
+    if (!preExamForLine(node, pos)) return false
+    if (Manager.useCubicBezier) {
+      return isPointOnBezierCurve(node.pos, node.endPos, pos)
+    } else {
+      return isPointOnCurve(node.stops, pos)
+    }
+  })
+}
+
+/**
+ * form the rect area for the line based on its start point and end point
+ * used with preExamForLine
+ * @param {ArrowNode} line
+ * @return {number[]}
+ */
+function formLineRect(line: ArrowNode): number[] {
+  const { x: startX, y: startY } = line.pos
+  const { x: endX, y: endY } = line.endPos
+  const $startX: number = Math.min(startX, endX)
+  const $startY: number = Math.min(startY, endY)
+  const width: number = Math.abs(startX - endX)
+  const height: number = Math.abs(startY - endY)
+  return [$startX, $startY, width, height]
+}
+
+/**
+ * if the (clicked) point is not within the rect area formed from line,
+ * it cannot be on line, therefore return false
+ * @param {ArrowNode} line
+ * @param {Pos} pos
+ * @return {boolean}
+ */
+function preExamForLine(line: ArrowNode, pos: Pos): boolean {
+  return isPointInPolygon(formLineRect(line), pos)
 }
